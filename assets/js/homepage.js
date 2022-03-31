93b9c297aa7869f1e58640c1db932c2fda7d53f3
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-03-31 16:35:01 -0400

    display response data on page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -14,6 +14,12 @@
 
 // fetch is used to make an HTTP request to the GitHub API; in turn, GitHub responded with JSON data - relationship: the request originated from the app and the response came from the GitHub's server
 
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+
 var getUserRepos = function(user) {
     // format the github api url
     
@@ -22,7 +28,8 @@ var getUserRepos = function(user) {
     // make a request to the url
     fetch(apiUrl).then(function(response){
         response.json().then(function(data){
-            console.log(data);
+            // when the response data is converted to JSON, it will be sent from getUserRepos() to displayRepos()
+            displayRepos(data, user);
         });
     });
 
@@ -38,4 +45,71 @@ var getUserRepos = function(user) {
             // console.log("outside");
 };
 
-getUserRepos();
+var formSubmitHandler = function(event) {
+    // stops the browser from performing the default action the event wants it to do; prevents the browser from sending the form's input data to a URL
+    event.preventDefault();
+
+    // get value from input element
+    // when we submit the form, we get the value from the <input> element via the nameInputEl DOM variable and store the value in its own variable called username; the .trim() is useful if we accidentally leave a leading or trailing space in the <input> element, such as " octocat" or "octocat "
+    var username = nameInputEl.value.trim();
+
+    // check that there's a value in that username variable; in the event we accidentally leave the <input> field blank, we wouldn't want to make an HTTP request without a username
+    if (username) {
+        // if there is in fact a value to username, we pass that data to getUserRepos() as an argument
+        getUserRepos(username);
+        // clear the form
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+}
+
+var displayRepos = function(repos, searchTerm) {
+    // clear old content
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+
+    // loop over repos
+    // in the for loop, we're taking each repository (repos[i]) and writing some of its data to the page
+    for (var i = 0; i < repos.length; i++) {
+        // format repo name
+        // we format the appearance of the name and the repository name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        // create a container for each repo
+        // we create and style a div element
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+        // create a span element to hold repository name
+        // we create a span to hold the formatted repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        // append to container
+        // we add that formatted repository name to the div
+        repoEl.appendChild(titleEl);
+
+        // create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        // check if current repo has issues or not
+        // use if statement to check how many issues the repository has; if the number is greater than zero, then we'll display the number of issues and add a red X icon next to it; if there are no issues, we'll display a blue check mark instead
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML = 
+                "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+
+        // append to container
+        repoEl.appendChild(statusEl);
+
+        // append container to the dom
+        // we add the entire div to the container we created earlier
+        repoContainerEl.appendChild(repoEl);
+    }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
